d757b71291b3ef93181f43045103aa0727d42246
Sam Skjonsberg <skoneberg@gmail.com>
2014-10-12 13:00:23 -0700

    Update build output.

--- src/client/paths.js
+++ src/client/paths.js
@@ -2,7 +2,7 @@ var src = 'src/';
 module.exports = {
   src: src,
   var: 'var',
-  build: 'build',
+  build: '../../build',
   js: src + 'main.js',
   assets: src + 'assets/**/*',
   less: src + 'styles.less',
